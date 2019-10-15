ce5787410034a5c051945a79076ced2e7ba1a653
Markus Reiter <me@reitermark.us>
2019-10-16 00:08:03 +0200

    Add flags to display socket/core mapping.

--- a01/osu.sh
+++ a01/osu.sh
@@ -6,5 +6,5 @@ module load openmpi/4.0.1
 
 export PATH="/scratch/c703429/osu-benchmark/libexec/osu-micro-benchmarks/mpi/pt2pt:${PATH}"
 
-mpiexec osu_latency
-mpiexec osu_bw
+mpiexec --display-map --display-allocation -n 2 osu_latency
+mpiexec --display-map --display-allocation -n 2 osu_bw
