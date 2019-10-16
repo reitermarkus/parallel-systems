ac03b67a81c4c692089f3069b805905846bdf9d6
Markus Reiter <me@reitermark.us>
2019-10-16 21:23:30 +0200

    Add `pi_mpi`.

---
+++ a02/e01/pi_mpi.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <mpi.h>
+
+using namespace std;
+using defer = shared_ptr<void>;
+
+void assert_mpi(string function, int error) {
+  if (error == 0) {
+    return;
+  }
+
+  cerr << function << " failed" << endl;
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+
+  defer _(nullptr, bind([]{
+    assert_mpi("MPI_Finalize", MPI_Finalize());
+  }));
+
+  int size;
+  assert_mpi("MPI_Comm_size", MPI_Comm_size(MPI_COMM_WORLD, &size));
+
+  int rank;
+  assert_mpi("MPI_Comm_rank", MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+
+  auto samples = 1000000000;
+
+  if (argc > 1) {
+    samples = strtol(argv[1], NULL, 10);
+  }
+
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_real_distribution<> dis(-1, 1);
+
+  auto inside = 0;
+
+  auto chunk_size = ((samples + (size - 1)) / size);
+  auto start = chunk_size * rank;
+  auto stop = start + chunk_size;
+  if (stop > samples) {
+    stop = samples;
+  }
+
+  for (auto i = start; i < stop; i++) {
+    auto x = dis(gen);
+    auto y = dis(gen);
+
+    if (x * x + y * y <= 1.0) {
+      inside++;
+    }
+  }
+
+  if (rank == 0) {
+    for (auto r = 1; r < size; r++) {
+      auto other_inside = 0;
+      MPI_Recv(&other_inside, 1, MPI_INT, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      inside += other_inside;
+    }
+
+    double pi = (double)inside / (double)samples * 4.0;
+    cout << "π = " << pi << endl;
+  } else {
+    MPI_Send(&inside, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  }
+
+  return EXIT_SUCCESS;
+}
+
