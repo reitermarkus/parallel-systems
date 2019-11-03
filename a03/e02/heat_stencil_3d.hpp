d28bd24b57523d7eae25a1edcd7440d20120bec9
Markus Reiter <me@reitermark.us>
2019-11-03 23:30:49 +0100

    Refactor 3D print function.

--- a03/e02/heat_stencil_3d.hpp
+++ a03/e02/heat_stencil_3d.hpp
@@ -19,9 +19,9 @@ inline void print_temperature(const vector<vector<vector<float>>>& mat) {
   const int d = 3;
 
   // step size in each dimension
-  const int sh = height / h;
-  const int sw = width / w;
-  const int sd = depth / d;
+  const float sh = height / static_cast<float>(h);
+  const float sw = width / static_cast<float>(w);
+  const float sd = depth / static_cast<float>(d);
 
   for (size_t k = 0; k < d; k++) {
     print_upper_wall(w);
@@ -33,9 +33,9 @@ inline void print_temperature(const vector<vector<vector<float>>>& mat) {
         // get max temperature in this tile
         float max_temp = 0;
 
-        for (size_t y = sh * i; y < sh * i + sh; y++) {
-          for (size_t x = sw * j; x < sw * j + sw; x++) {
-            for (size_t z = sd * k; z < sd * k + sd; z++) {
+        for (size_t y = sh * i; y < min<size_t>(sh * i + sh, height); y++) {
+          for (size_t x = sw * j; x < min<size_t>(sw * j + sw, width); x++) {
+            for (size_t z = sd * k; z < min<size_t>(sd * k + sd, depth); z++) {
               max_temp =  max(max_temp, mat[y][x][z]);
             }
           }
