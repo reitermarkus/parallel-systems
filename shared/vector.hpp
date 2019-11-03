baabf743477b7ed36b334535ab429f84ad32a113
Markus Reiter <me@reitermark.us>
2019-11-03 23:30:25 +0100

    Add vector helper for debugging.

---
+++ shared/vector.hpp
@@ -0,0 +1,9 @@
+template <typename T>
+std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
+  if ( !v.empty() ) {
+    out << '[';
+    std::copy (v.begin(), v.end(), std::ostream_iterator<T>(out, ", "));
+    out << "]";
+  }
+  return out;
+}
