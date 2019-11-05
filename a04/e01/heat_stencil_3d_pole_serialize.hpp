d634fb163c17660f09ad2e4183d1221739e2cc04
Markus Reiter <me@reitermark.us>
2019-11-05 21:41:25 +0100

    Refactor serialize headers.

--- a04/e01/heat_stencil_3d_pole_serialize.hpp
+++ a04/e01/heat_stencil_3d_pole_serialize.hpp
@@ -1,78 +1,9 @@
 #include <vector>
 
+#include "heat_stencil_3d_serialize.hpp"
 #include "../../shared/boost.hpp"
 
-class x_direction {
-  vector<vector<vector<float>>>& matrix;
-  size_t x;
-  size_t height;
-  size_t depth;
-
-  friend class boost::serialization::access;
-
-  template<class Archive>
-  void serialize(Archive& ar, const unsigned int version) {
-    (void)version;
-
-    for (size_t y = 0; y < height; y++) {
-      for (size_t z = 0; z < depth; z++) {
-        ar & matrix[y][x][z];
-      }
-    }
-  }
-
-  public:
-    x_direction(vector<vector<vector<float>>>& _matrix, size_t _x)
-      : matrix(_matrix), x(_x), height(_matrix.size()), depth(_matrix[0][0].size()) {}
-};
-
-class y_direction {
-  vector<vector<vector<float>>>& matrix;
-  size_t y;
-  size_t width;
-  size_t depth;
-
-  friend class boost::serialization::access;
-
-  template<class Archive>
-  void serialize(Archive& ar, const unsigned int version) {
-    (void)version;
-
-    for (size_t x = 0; x < width; x++) {
-      for (size_t z = 0; z < depth; z++) {
-        ar & matrix[y][x][z];
-      }
-    }
-  }
-
-  public:
-    y_direction(vector<vector<vector<float>>>& _matrix, size_t _y)
-      : matrix(_matrix), y(_y), width(_matrix[0].size()), depth(_matrix[0][0].size()) {}
-};
-
-class z_direction {
-  vector<vector<vector<float>>>& matrix;
-  size_t z;
-  size_t height;
-  size_t width;
-
-  friend class boost::serialization::access;
-
-  template<class Archive>
-  void serialize(Archive& ar, const unsigned int version) {
-    (void)version;
-
-    for (size_t y = 0; y < height; y++) {
-      for (size_t x = 0; x < width; x++) {
-        ar & matrix[y][x][z];
-      }
-    }
-  }
-
-  public:
-    z_direction(vector<vector<vector<float>>>& _matrix, size_t _z)
-      : matrix(_matrix), z(_z), height(_matrix.size()), width(_matrix[0].size()) {}
-};
+using namespace std;
 
 class collector {
   vector<vector<vector<float>>>& matrix;
