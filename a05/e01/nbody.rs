c47a9b5659da631fda3b4ceaeb505460c326cbab
Markus Reiter <me@reitermark.us>
2019-11-12 09:44:09 +0100

    Refactor Rust version of N-Body simulation.

--- a05/e01/nbody.rs
+++ a05/e01/nbody.rs
@@ -3,6 +3,8 @@
 use std::{env, thread::sleep, time::Duration};
 use rand::Rng;
 
+const G: f32 = 1.0;
+
 #[derive(Debug)]
 struct Particle {
   position: (f32, f32),
@@ -11,30 +13,15 @@ struct Particle {
 }
 
 impl Particle {
-  pub fn distance_to(&self, other: &Self) -> f32 {
-    let dx = self.position.0 - other.position.0;
-    let dy = self.position.1 - other.position.1;
+  pub fn generate() -> Self {
+    let mut rng = rand::thread_rng();
 
-    (dx.powf(2.0) + dy.powf(2.0)).sqrt()
+    Particle {
+      position: (rng.gen_range(-1.0, 1.0), rng.gen_range(-1.0, 1.0)),
+      velocity: (rng.gen_range(-1.0, 1.0), rng.gen_range(-1.0, 1.0)),
+      mass: rng.gen_range(0.01, 10.0),
+    }
   }
-}
-
-const G: f32 = 1.0;
-
-fn generate_particle() -> Particle {
-  let mut rng = rand::thread_rng();
-
-  Particle {
-    position: (rng.gen_range(-1.0, 1.0), rng.gen_range(-1.0, 1.0)),
-    velocity: (rng.gen_range(-1.0, 1.0), rng.gen_range(-1.0, 1.0)),
-    mass: rng.gen_range(0.01, 10.0),
-  }
-}
-
-fn calculate_force(p1: &Particle, p2: &Particle) -> f32 {
-  let radius = p1.distance_to(p2);
-  assert!(radius > 0.0);
-  G * (p1.mass * p2.mass) / radius.powf(2.0)
 }
 
 fn visualize(particles: &[Particle]) {
@@ -88,6 +75,37 @@ fn visualize(particles: &[Particle]) {
   println!("┘");
 }
 
+#[inline(always)]
+fn advance(particles: &mut [Particle], dt: f32) {
+  for i in 0..particles.len() {
+    for j in (i + 1)..particles.len() {
+      let dx = particles[i].position.0 - particles[j].position.0;
+      let dy = particles[i].position.1 - particles[j].position.1;
+
+      let radius = (dx.powf(2.0) + dy.powf(2.0)).sqrt();
+      debug_assert!(radius > 0.0);
+      let force = G * (particles[i].mass * particles[i].mass) / radius.powf(2.0);
+
+      let velocity_i = force * dt / particles[j].mass;
+      let velocity_j = force * dt / particles[i].mass;
+
+      particles[i].velocity = (
+        particles[i].velocity.0 - dx * velocity_j,
+        particles[i].velocity.1 - dy * velocity_j,
+      );
+      particles[j].velocity = (
+        particles[j].velocity.0 + dx * velocity_i,
+        particles[j].velocity.1 + dy * velocity_i,
+      );
+    }
+
+    particles[i].position = (
+      particles[i].position.0 + particles[i].velocity.0 * dt,
+      particles[i].position.1 + particles[i].velocity.1 * dt,
+    );
+  }
+}
+
 fn main() {
   let samples = env::args().nth(1)
     .map(|n| n.parse().expect("Failed to parse argument"))
@@ -97,36 +115,12 @@ fn main() {
 
   let time_steps = 1_000;
 
-  let mut particles = (0..samples).map(|_| generate_particle()).collect::<Vec<_>>();
+  let mut particles = (0..samples).map(|_| Particle::generate()).collect::<Vec<_>>();
 
   visualize(&particles);
 
   for t in 0..time_steps {
-    for i in 0..samples {
-      for j in (i + 1)..samples {
-        let dx = particles[i].position.0 - particles[j].position.0;
-        let dy = particles[i].position.1 - particles[j].position.1;
-
-        let force = calculate_force(&particles[i], &particles[j]);
-
-        let velocity_i = force * dt / particles[j].mass;
-        let velocity_j = force * dt / particles[i].mass;
-
-        particles[i].velocity = (
-          particles[i].velocity.0 - dx * velocity_j,
-          particles[i].velocity.1 - dy * velocity_j,
-        );
-        particles[j].velocity = (
-          particles[j].velocity.0 + dx * velocity_i,
-          particles[j].velocity.1 + dy * velocity_i,
-        );
-      }
-
-      particles[i].position = (
-        particles[i].position.0 + particles[i].velocity.0 * dt,
-        particles[i].position.1 + particles[i].velocity.1 * dt,
-      );
-    }
+    advance(&mut particles, dt);
 
     sleep(Duration::from_millis(50));
     println!("t = {}", t);
