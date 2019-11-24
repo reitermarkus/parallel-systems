41900d78aecefa58d3af07937c805b41a20ce3a7
Markus Reiter <me@reitermark.us>
2019-11-24 22:55:04 +0100

    Refactor Makefile for OpenMP.

--- shared/shared.mk
+++ shared/shared.mk
@@ -1,16 +1,31 @@
-CXX := mpic++
+MPI_CXX ?= mpic++
 
 CPPFLAGS ?= -O3 -Wall -Wextra -pedantic
 CXXFLAGS ?= -std=c++17
 
-LDFLAGS += -lboost_mpi -lboost_serialization
+ifneq ($(OS), Windows_NT)
+  OS ?= $(shell uname -s)
+endif
+
+ifeq ($(OS), Darwin)
+  ifeq ($(shell brew ls libomp &>/dev/null; echo $$?), 0)
+    LIBOMP_PREFIX = $(shell brew --prefix libomp)
+    OPENMP_CPPFLAGS += -I$(LIBOMP_PREFIX)/include
+    OPENMP_LDFLAGS += -L$(LIBOMP_PREFIX)/lib
+  else
+    $(error "OpenMP is not installed, run `brew install libomp`")
+  endif
+endif
+
+OPENMP_LDFLAGS += -Xpreprocessor -fopenmp
+MPI_LDFLAGS += -lboost_mpi -lboost_serialization
 
 ifneq ("$(wildcard $(HOME)/.local/include)", "")
-	CPPFLAGS += -I"$(HOME)/.local/include"
+	MPI_CPPFLAGS += -I"$(HOME)/.local/include"
 endif
 
 ifneq ("$(wildcard $(HOME)/.local/lib)", "")
-	LDFLAGS += -L"$(HOME)/.local/lib" -Wl,-rpath,"$(HOME)/.local/lib"
+	MPI_LDFLAGS += -L"$(HOME)/.local/lib" -Wl,-rpath,"$(HOME)/.local/lib"
 endif
 
 ifeq ($(DEBUG),1)
@@ -22,8 +37,20 @@ TARGETS ?= $(sort $(patsubst %.cpp,%,$(wildcard *_omp.cpp *_seq.cpp *_mpi.cpp)))
 .PHONY: all
 all: $(TARGETS)
 
-$(TARGETS): %: %.o
+%_seq: %_seq.o
 	$(CXX) $^ $(LDFLAGS) -o $@
+
+%_mpi.o: %_mpi.cpp
+	$(MPI_CXX) $(CXXFLAGS) $(CPPFLAGS) $(MPI_CPPFLAGS) -c -o $@ $^
+
+%_mpi: %_mpi.o
+	$(MPI_CXX) $^ $(LDFLAGS) $(MPI_LDFLAGS) -o $@
+
+%_omp.o: %_omp.cpp
+	$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPENMP_CPPFLAGS) -c -o $@ $^
+
+%_omp: %_omp.o
+	$(CXX) $^ $(LDFLAGS) $(OPENMP_LDFLAGS) -o $@
 
 .PHONY: clean
 clean:
