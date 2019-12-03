49d1fea803138c640ce9f95af40745750401b3fd
Markus Reiter <me@reitermark.us>
2019-12-03 06:01:44 +0100

    Port matrix multiplcation to C++.

--- a08/e02/mat_mul_omp.cpp
+++ a08/e02/mat_mul_omp.cpp
@@ -1,7 +1,26 @@
-#include <stdio.h>
-#include "matrix.h"
+#include <iostream>
+#include <vector>
 
 #include "../../shared/parse_ull.hpp"
+
+using namespace std;
+
+int verify(const vector<vector<float>>& mat) {
+  cout << "Verification: ";
+
+  for (size_t i = 0; i < mat.size(); i++) {
+    for (size_t j = 0; j < mat[0].size(); j++) {
+      if (mat[i][j] != i * j) {
+
+        cout << "FAILED" << endl;
+        return EXIT_FAILURE;
+      };
+    }
+  }
+
+  cout << "OK" << endl;
+  return EXIT_SUCCESS;
+}
 
 int main(int argc, char** argv) {
   size_t n = 1000;
@@ -10,36 +29,38 @@ int main(int argc, char** argv) {
     n = parse_ull(argv[1]);
   }
 
-  printf("Matrix Multiplication with n=%zu\n", n);
+  cout << "Matrix Multiplication with n = " << n << endl;
 
-  // -------------------- SETUP -------------------- //
-  Matrix mtx_a = create_matrix(n, n);
-  Matrix mtx_b = create_matrix(n, n);
-
-  fill_matrices(mtx_a, mtx_b, n, n);
-
-  Matrix mtx_res = create_matrix(n, n);
-
-  // ------------------- COMPUTE -------------------- //
+  // Create a matrix depending on the row/column so we can verify it.
+  auto mat_a = vector<vector<float>>(n, vector<float>(n));
   #pragma omp parallel for
-  for (size_t i = 0; i < n; i++) {
-    for (size_t j = 0; j < n; j++) {
-      value_t sum = 0;
-      for (size_t k = 0; k < n; k++) {
-        sum += mtx_a[i * n + k] * mtx_b[k * n + j];
-      }
-      mtx_res[i * n + j] = sum;
+  for (size_t i = 0; i < mat_a.size(); i++) {
+    for (size_t j = 0; j < mat_a[0].size(); j++) {
+      mat_a[i][j] = i * j;
     }
   }
 
-  // ------------------- CHECK ------------------- //
-  bool success = check(mtx_res, n, n);
-  printf("Verification: %s\n", success ? "OK" : "FAILED");
+ // Create an identity matrix.
+  auto mat_b = vector<vector<float>>(mat_a[0].size(), vector<float>(mat_a[0].size()));
+  #pragma omp parallel for
+  for (size_t i = 0; i < mat_b.size(); i++) {
+    for (size_t j = 0; j < mat_b[0].size(); j++) {
+      mat_b[i][j] = i == j ? 1 : 0;
+    }
+  }
 
-  // ----------------- CLEAN UP ------------------ //
-  free(mtx_a);
-  free(mtx_b);
-  free(mtx_res);
+  auto mat_res = vector<vector<float>>(mat_a.size(), vector<float>(mat_b[0].size()));
 
-  return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
+  #pragma omp parallel for
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < n; j++) {
+      float sum = 0;
+      for (size_t k = 0; k < n; k++) {
+        sum += mat_a[i][k] * mat_b[k][j];
+      }
+      mat_res[i][j] = sum;
+    }
+  }
+
+  return verify(mat_res);
 }
