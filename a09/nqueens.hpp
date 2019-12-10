a93062d42e5dbaee288722f7294829de43e19d8f
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2019-12-10 20:51:48 +0100

    Move `Queen` struct into header.

--- a09/nqueens.hpp
+++ a09/nqueens.hpp
@@ -5,6 +5,30 @@
 
 using namespace std;
 
+struct Queen {
+  size_t row;
+  size_t column;
+
+  Queen(size_t row, size_t column) {
+    this->row = row;
+    this->column = column;
+  }
+
+  bool can_be_killed_by(const vector<Queen>& other_queens) {
+    for (auto queen: other_queens) {
+      ssize_t row_diff = abs(ssize_t(queen.row) - ssize_t(this->row));
+      ssize_t col_diff = abs(ssize_t(queen.column) - ssize_t(this->column));
+
+      // horizontal || diagonal
+      if (row_diff == 0 || row_diff == col_diff) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
+
 void print(const vector<size_t>& board, int queens) {
   for (int i = 0; i < queens; i++) {
     for (int j = 0; j < queens; j++)
--- a09/nqueens_seq.cpp
+++ a09/nqueens_seq.cpp
@@ -4,30 +4,6 @@
 
 #include "nqueens.hpp"
 
-struct Queen {
-  size_t row;
-  size_t column;
-
-  Queen(size_t row, size_t column) {
-    this->row = row;
-    this->column = column;
-  }
-
-  bool can_be_killed_by(const vector<Queen>& other_queens) {
-    for (auto queen: other_queens) {
-      ssize_t row_diff = abs(ssize_t(queen.row) - ssize_t(this->row));
-      ssize_t col_diff = abs(ssize_t(queen.column) - ssize_t(this->column));
-
-      // horizontal || diagonal
-      if (row_diff == 0 || row_diff == col_diff) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-};
-
 size_t solve(
   #if DEBUG
     vector<size_t>& board,
