f74876cfe4c0a32cf37857c1f7913841c62335f7
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-01-13 01:27:33 +0100

    Parallelize `rprj3`.

--- a11/real.c
+++ a11/real.c
@@ -588,59 +588,65 @@ static void rprj3(void *or, int m1k, int m2k, int m3k,
   double (*r)[m2k][m1k] = (double (*)[m2k][m1k])or;
   double (*s)[m2j][m1j] = (double (*)[m2j][m1j])os;
 
-  int j3, j2, j1, i3, i2, i1, d1, d2, d3, j;
+  #pragma omp parallel
+  {
+    int j3, j2, j1, i3, i2, i1, d1, d2, d3, j;
 
-  double x1[M], y1[M], x2, y2;
+    double x1[M], y1[M], x2, y2;
 
-  if (timeron) timer_start(T_rprj3);
-  if (m1k == 3) {
-    d1 = 2;
-  } else {
-    d1 = 1;
-  }
+    if (timeron) timer_start(T_rprj3);
+    if (m1k == 3) {
+      d1 = 2;
+    } else {
+      d1 = 1;
+    }
 
-  if (m2k == 3) {
-    d2 = 2;
-  } else {
-    d2 = 1;
-  }
+    if (m2k == 3) {
+      d2 = 2;
+    } else {
+      d2 = 1;
+    }
 
-  if (m3k == 3) {
-    d3 = 2;
-  } else {
-    d3 = 1;
-  }
+    if (m3k == 3) {
+      d3 = 2;
+    } else {
+      d3 = 1;
+    }
 
-  for (j3 = 1; j3 < m3j-1; j3++) {
-    i3 = 2*j3-d3;
-    for (j2 = 1; j2 < m2j-1; j2++) {
-      i2 = 2*j2-d2;
+    #pragma omp for
+    for (j3 = 1; j3 < m3j-1; j3++) {
+      i3 = 2*j3-d3;
+      for (j2 = 1; j2 < m2j-1; j2++) {
+        i2 = 2*j2-d2;
 
-      for (j1 = 1; j1 < m1j; j1++) {
-        i1 = 2*j1-d1;
-        x1[i1] = r[i3+1][i2  ][i1] + r[i3+1][i2+2][i1]
-               + r[i3  ][i2+1][i1] + r[i3+2][i2+1][i1];
-        y1[i1] = r[i3  ][i2  ][i1] + r[i3+2][i2  ][i1]
-               + r[i3  ][i2+2][i1] + r[i3+2][i2+2][i1];
-      }
+        for (j1 = 1; j1 < m1j; j1++) {
+          i1 = 2*j1-d1;
+          x1[i1] = r[i3+1][i2  ][i1] + r[i3+1][i2+2][i1]
+                + r[i3  ][i2+1][i1] + r[i3+2][i2+1][i1];
+          y1[i1] = r[i3  ][i2  ][i1] + r[i3+2][i2  ][i1]
+                + r[i3  ][i2+2][i1] + r[i3+2][i2+2][i1];
+        }
 
-      for (j1 = 1; j1 < m1j-1; j1++) {
-        i1 = 2*j1-d1;
-        y2 = r[i3  ][i2  ][i1+1] + r[i3+2][i2  ][i1+1]
-           + r[i3  ][i2+2][i1+1] + r[i3+2][i2+2][i1+1];
-        x2 = r[i3+1][i2  ][i1+1] + r[i3+1][i2+2][i1+1]
-           + r[i3  ][i2+1][i1+1] + r[i3+2][i2+1][i1+1];
-        s[j3][j2][j1] =
-                0.5 * r[i3+1][i2+1][i1+1]
-              + 0.25 * (r[i3+1][i2+1][i1] + r[i3+1][i2+1][i1+2] + x2)
-              + 0.125 * (x1[i1] + x1[i1+2] + y2)
-              + 0.0625 * (y1[i1] + y1[i1+2]);
+        for (j1 = 1; j1 < m1j-1; j1++) {
+          i1 = 2*j1-d1;
+          y2 = r[i3  ][i2  ][i1+1] + r[i3+2][i2  ][i1+1]
+            + r[i3  ][i2+2][i1+1] + r[i3+2][i2+2][i1+1];
+          x2 = r[i3+1][i2  ][i1+1] + r[i3+1][i2+2][i1+1]
+            + r[i3  ][i2+1][i1+1] + r[i3+2][i2+1][i1+1];
+          s[j3][j2][j1] =
+                  0.5 * r[i3+1][i2+1][i1+1]
+                + 0.25 * (r[i3+1][i2+1][i1] + r[i3+1][i2+1][i1+2] + x2)
+                + 0.125 * (x1[i1] + x1[i1+2] + y2)
+                + 0.0625 * (y1[i1] + y1[i1+2]);
+        }
       }
     }
+
+    j = k - 1;
   }
+
   if (timeron) timer_stop(T_rprj3);
 
-  j = k-1;
   comm3(s, m1j, m2j, m3j);
 
   if (debug_vec[0] >= 1) {
