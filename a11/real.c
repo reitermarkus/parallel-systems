fc1f3a019f6a985c88e0d717d114ce2cc5db7239
Markus Reiter <me@reitermark.us>
2020-01-13 01:10:11 +0100

    Add pragma.

--- a11/real.c
+++ a11/real.c
@@ -1226,6 +1226,9 @@ static void zero3(void *oz, int n1, int n2, int n3)
 
   int i1, i2, i3;
 
+  #ifdef _OPENMP
+  #pragma omp parallel for
+  #endif
   for (i3 = 0; i3 < n3; i3++) {
     for (i2 = 0; i2 < n2; i2++) {
       for (i1 = 0; i1 < n1; i1++) {
